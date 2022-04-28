b207a0a014d00a9ccf850eb7b2b33a66db1c1c16
Lucas Spencer <lucas.spencer14@gmail.com>
2022-04-28 14:44:44 -0600

    again

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ var rollbar = new Rollbar({
 rollbar.log('Hello world!')
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
     rollbar.info('html file served successfully.')
 })
  
